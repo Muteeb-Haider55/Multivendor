ad7a8fadccecbba13a5870250fca1c1146bfe197
Muteeb-Haider55 <b-27240@student.usa.edu.pk>
2025-10-08 01:53:49 -0700

    feat: backend url added

--- frontend/server.js
+++ frontend/server.js
@@ -1,2 +1,2 @@
-export const server = "http://localhost:3000/api/v2";
-export const backend_url = "http://localhost:3000";
+export const server = "https://multivendor-pi.vercel.app/api/v2";
+export const backend_url = "https://multivendor-pi.vercel.app";
