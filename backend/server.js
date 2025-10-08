7107eca9a3516c82506fd327a90a7a065b61c018
Muteeb-Haider55 <b-27240@student.usa.edu.pk>
2025-10-08 05:32:36 -0700

    feat: cloudinary configration

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require("./app.js");
 const connecDatabase = require("./db/Database.js");
+const cloudinary = require("cloudinary");
 
 //Handeling unCaught Execution
 process.on("uncaughtException", (err) => {
@@ -15,6 +16,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 }
 //connect db
 connecDatabase();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 //create server
 const server = app.listen(process.env.PORT || 3000, () => {
